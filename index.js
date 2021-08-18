335352818ca2224b857519715dc6b5908db48e2c
Jin <alpsboy1996@gmail.com>
2021-08-19 01:13:29 +0900

    Add existing file

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const leftArrow = document.querySelector(".fa-arrow-left");
 const rightArrow = document.querySelector(".fa-arrow-right");
 const tableBody = document.querySelector("tbody");
 
-const monthArray = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
-const dayArray = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+const MONTHARRAY = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+const DAYARRAY = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
 
 const today = new Date();
 let month = today.getMonth();
 let year = today.getFullYear();
 
 // Sunday = 0 and Saturday = 6. This sets current day 
-day.innerText = dayArray[today.getDay()];
+day.innerText = DAYARRAY[today.getDay()];
 date.innerText = today.getDate();
-todayMonthYear.innerText = `${monthArray[month]} ${year}`;
+todayMonthYear.innerText = `${MONTHARRAY[month]} ${year}`;
 
 // Function to generate dates and places each dates on the right table date.
 function generateCalendar() {
@@ -23,56 +23,69 @@ function generateCalendar() {
   const numberOfDays = 32 - new Date(year,month,32).getDate();
   let num = 1;
   for (let i = 0; i < 6; i++) {
-    const tr = document.createElement("tr");
+    const tableRow = document.createElement("tr");
     for (let j = 0; j < 7; j++) {
       if (i === 0 && j < startOfMonth) {
-        const td = document.createElement("td");
-        tr.appendChild(td);
+        const tableData = document.createElement("td");
+        tableRow.appendChild(tableData);
       } else if ( num > numberOfDays) {
         break;
       }else {
-        const td = document.createElement("td");
-        td.textContent = num;
-        if (num == today.getDate() && month == today.getMonth()) {
-          td.classList.add("highlighter");
+        const tableData = document.createElement("td");
+        tableData.textContent = num;
+        if (num === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
+          tableData.classList.add("highlighter");
         }
-        tr.appendChild(td);
+        tableRow.appendChild(tableData);
         num++;
-        td.addEventListener("click",showClickedDate => {
-          date.innerText = td.innerText;
-          day.innerText = dayArray[new Date(year,month,date.innerText).getDay()]; 
+        tableData.addEventListener("click",showClickedDate => {
+          date.innerText = tableData.innerText;
+          day.innerText = DAYARRAY[new Date(year,month,date.innerText).getDay()]; 
         });
       }
     }
-    tableBody.appendChild(tr);
+    tableBody.appendChild(tableRow);
+  }
+}
+
+// Function to show next Month or previous Month 1st day and date.
+function showNextMonthDate() {
+  if (month !== today.getMonth() || year !== today.getFullYear()) {
+    day.innerText = DAYARRAY[new Date(year,month).getDay()];
+    date.innerText = new Date(year,month).getDate();
+  }else{
+    day.innerText = DAYARRAY[today.getDay()];
+    date.innerText = today.getDate();
   }
 }
 
 // Function to show previous month.
 function showPreviousCalendar() {;
-  if (month == 0) {
+  if (month === 0) {
     month = 11;
-    year = year - 1;
-    todayMonthYear.innerText = `${monthArray[month]} ${year}`;
+    year--;
+    todayMonthYear.innerText = `${MONTHARRAY[month]} ${year}`;
   }else {
-    month = month - 1;
-    todayMonthYear.innerText = `${monthArray[month]} ${year}`;
+    month--;
+    todayMonthYear.innerText = `${MONTHARRAY[month]} ${year}`;
   }
   tableBody.innerHTML = "";
+  showNextMonthDate();
   generateCalendar();
 }
 
 // Function to show next month.
 function showNextCalendar() {
-  if (month == 11) {
+  if (month === 11) {
     month = 0;
-    year = year + 1;
-    todayMonthYear.innerText = `${monthArray[month]} ${year}`;
+    year++;
+    todayMonthYear.innerText = `${MONTHARRAY[month]} ${year}`;
   }else {
-    month = month + 1;
-    todayMonthYear.innerText = `${monthArray[month]} ${year}`;
+    month++;
+    todayMonthYear.innerText = `${MONTHARRAY[month]} ${year}`;
   }
   tableBody.innerHTML = "";
+  showNextMonthDate();
   generateCalendar();
 }
 
